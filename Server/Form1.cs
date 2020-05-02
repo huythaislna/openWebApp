46196847ecd8bca23fbdc931e241b3660616b2f6
tides92 <huythaifcb@gmail.com>
2020-05-02 23:37:22 +0700

    Template function server

--- Server/Form1.cs
+++ Server/Form1.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace Server
 {
     public partial class Form1 : Form
     {
+        IPAddress ipAddress = IPAddress.Any;
+        Int32 port = 8080;
+        TcpListener Server;
+        Socket Client;
+        NetworkStream clientStream;
         public Form1()
         {
             InitializeComponent();
-        }
+        
+        //Process everything of server (use for button click)
+        private void Process() { }
+        //Setup port, ip,.... and start server
+        private void Setup() { }
+        //listen to client for sending and receiving
+        private void Listen(object clientObj) { }
+        //waiting for client and accept connection
+        private void Connect() { }
+        //send a message
+        private void SendData(string message) { }
+        //get message from client
+        private string ReceiveData() { }
+        //website is working correctly.
+        private bool IsValid(string url) { }
+        //Transfer input of user to a valid url(add "http, https")
+
+        private string ValidateUrl(string url) { }
+        //Open default Browser
+        private void OpenBrowser(string url) { }
     }
 }
