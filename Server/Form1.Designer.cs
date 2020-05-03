b35bf7cb2838227b56ece75634d6a2ebf1e80abd
tides92 <huythaifcb@gmail.com>
2020-05-03 23:46:41 +0700

    Server GUI fixing

--- Server/Form1.Designer.cs
+++ Server/Form1.Designer.cs
@@ -28,91 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.log_tb = new System.Windows.Forms.TextBox();
+            this.banner_pic = new System.Windows.Forms.PictureBox();
+            this.listen_bt = new System.Windows.Forms.Button();
+            this.stop_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.banner_pic)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // log_tb
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.DeepSkyBlue;
-            this.label1.Location = new System.Drawing.Point(218, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(220, 90);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Hello";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.log_tb.Location = new System.Drawing.Point(100, 256);
+            this.log_tb.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.log_tb.Multiline = true;
+            this.log_tb.Name = "log_tb";
+            this.log_tb.ReadOnly = true;
+            this.log_tb.Size = new System.Drawing.Size(457, 145);
+            this.log_tb.TabIndex = 4;
+            this.log_tb.Text = "Waiting for a connection...";
+            this.log_tb.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // label2
+            // banner_pic
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label2.Location = new System.Drawing.Point(251, 98);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(141, 22);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Good to see you";
+            this.banner_pic.Image = global::Server.Properties.Resources.Open_Web_Away_;
+            this.banner_pic.Location = new System.Drawing.Point(130, 58);
+            this.banner_pic.Name = "banner_pic";
+            this.banner_pic.Size = new System.Drawing.Size(700, 191);
+            this.banner_pic.TabIndex = 8;
+            this.banner_pic.TabStop = false;
+            this.banner_pic.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // button1
+            // listen_bt
             // 
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(393, 321);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 56);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Exit";
-            this.button1.UseVisualStyleBackColor = true;
+            this.listen_bt.Location = new System.Drawing.Point(603, 256);
+            this.listen_bt.Name = "listen_bt";
+            this.listen_bt.Size = new System.Drawing.Size(174, 58);
+            this.listen_bt.TabIndex = 9;
+            this.listen_bt.Text = "Listen";
+            this.listen_bt.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // stop_button
             // 
-            this.textBox2.Location = new System.Drawing.Point(61, 137);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(563, 179);
-            this.textBox2.TabIndex = 4;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(189, 320);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(115, 56);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Listen";
-            this.button2.UseVisualStyleBackColor = true;
+            this.stop_button.Location = new System.Drawing.Point(603, 343);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(174, 58);
+            this.stop_button.TabIndex = 10;
+            this.stop_button.Text = "Stop";
+            this.stop_button.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(685, 406);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClientSize = new System.Drawing.Size(913, 500);
+            this.Controls.Add(this.stop_button);
+            this.Controls.Add(this.listen_bt);
+            this.Controls.Add(this.banner_pic);
+            this.Controls.Add(this.log_tb);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Server";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.banner_pic)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox log_tb;
+        private System.Windows.Forms.PictureBox banner_pic;
+        private System.Windows.Forms.Button listen_bt;
+        private System.Windows.Forms.Button stop_button;
     }
 }
 
