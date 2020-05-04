371675b07a58a772263089401319fbefd4a13d8a
Hiếu Nguyễn Đức <duchieu262@gmail.com>
2020-05-04 14:18:34 +0700

    Server

---
+++ Client/CLIENT.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Client
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Client));
+            this.connect_bt = new System.Windows.Forms.Button();
+            this.status_lb = new System.Windows.Forms.Label();
+            this.banner_pic = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.url = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.banner_pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connect_bt
+            // 
+            this.connect_bt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connect_bt.ForeColor = System.Drawing.Color.Black;
+            this.connect_bt.Location = new System.Drawing.Point(667, 236);
+            this.connect_bt.Margin = new System.Windows.Forms.Padding(4);
+            this.connect_bt.Name = "connect_bt";
+            this.connect_bt.Size = new System.Drawing.Size(111, 47);
+            this.connect_bt.TabIndex = 4;
+            this.connect_bt.Text = "Connect";
+            this.connect_bt.UseVisualStyleBackColor = true;
+            this.connect_bt.Click += new System.EventHandler(this.connect_bt_Click);
+            // 
+            // status_lb
+            // 
+            this.status_lb.AutoSize = true;
+            this.status_lb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status_lb.ForeColor = System.Drawing.Color.ForestGreen;
+            this.status_lb.Location = new System.Drawing.Point(233, 407);
+            this.status_lb.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.status_lb.Name = "status_lb";
+            this.status_lb.Size = new System.Drawing.Size(0, 25);
+            this.status_lb.TabIndex = 12;
+            // 
+            // banner_pic
+            // 
+            this.banner_pic.Image = ((System.Drawing.Image)(resources.GetObject("banner_pic.Image")));
+            this.banner_pic.Location = new System.Drawing.Point(122, 12);
+            this.banner_pic.Name = "banner_pic";
+            this.banner_pic.Size = new System.Drawing.Size(656, 200);
+            this.banner_pic.TabIndex = 13;
+            this.banner_pic.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Client.Properties.Resources.search32x32;
+            this.button1.Location = new System.Drawing.Point(667, 350);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 41);
+            this.button1.TabIndex = 16;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // url
+            // 
+            this.url.Enabled = false;
+            this.url.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.url.HideSelection = false;
+            this.url.Location = new System.Drawing.Point(238, 350);
+            this.url.Margin = new System.Windows.Forms.Padding(4);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(356, 27);
+            this.url.TabIndex = 14;
+            this.url.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox4_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "IP Address";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(238, 236);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(235, 27);
+            this.textBox2.TabIndex = 18;
+            this.textBox2.Text = "127.0.0.1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(238, 293);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(103, 27);
+            this.textBox3.TabIndex = 20;
+            this.textBox3.Text = "8080";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(124, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(124, 350);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "URL";
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 492);
+            this.Controls.Add(this.status_lb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.banner_pic);
+            this.Controls.Add(this.connect_bt);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Client";
+            this.RightToLeftLayout = true;
+            this.Text = "Client";
+            ((System.ComponentModel.ISupportInitialize)(this.banner_pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button connect_bt;
+        private System.Windows.Forms.Label status_lb;
+        private System.Windows.Forms.PictureBox banner_pic;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
