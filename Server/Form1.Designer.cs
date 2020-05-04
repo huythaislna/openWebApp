9aa1cc0b9a7184a4a83e2eb87fcacc6c67b7e1c6
huythaislna <huythaislna@gmail.com>
2020-05-04 08:26:29 +0700

    add port and fixing GUI

--- Server/Form1.Designer.cs
+++ Server/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Server
 {
-    partial class Form1
+    partial class Server
     {
         /// <summary>
         /// Required designer variable.
@@ -30,64 +30,85 @@
         {
             this.log_tb = new System.Windows.Forms.TextBox();
             this.banner_pic = new System.Windows.Forms.PictureBox();
-            this.listen_bt = new System.Windows.Forms.Button();
+            this.start_bt = new System.Windows.Forms.Button();
             this.stop_button = new System.Windows.Forms.Button();
+            this.port_tb = new System.Windows.Forms.TextBox();
+            this.port_lb = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.banner_pic)).BeginInit();
             this.SuspendLayout();
             // 
             // log_tb
             // 
-            this.log_tb.Location = new System.Drawing.Point(100, 256);
-            this.log_tb.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.log_tb.Location = new System.Drawing.Point(128, 292);
+            this.log_tb.Margin = new System.Windows.Forms.Padding(4);
             this.log_tb.Multiline = true;
             this.log_tb.Name = "log_tb";
             this.log_tb.ReadOnly = true;
             this.log_tb.Size = new System.Drawing.Size(457, 145);
             this.log_tb.TabIndex = 4;
             this.log_tb.Text = "Waiting for a connection...";
-            this.log_tb.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // banner_pic
             // 
             this.banner_pic.Image = global::Server.Properties.Resources.Open_Web_Away_;
-            this.banner_pic.Location = new System.Drawing.Point(130, 58);
+            this.banner_pic.Location = new System.Drawing.Point(128, 28);
             this.banner_pic.Name = "banner_pic";
             this.banner_pic.Size = new System.Drawing.Size(700, 191);
             this.banner_pic.TabIndex = 8;
             this.banner_pic.TabStop = false;
-            this.banner_pic.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // listen_bt
+            // start_bt
             // 
-            this.listen_bt.Location = new System.Drawing.Point(603, 256);
-            this.listen_bt.Name = "listen_bt";
-            this.listen_bt.Size = new System.Drawing.Size(174, 58);
-            this.listen_bt.TabIndex = 9;
-            this.listen_bt.Text = "Listen";
-            this.listen_bt.UseVisualStyleBackColor = true;
+            this.start_bt.Location = new System.Drawing.Point(601, 292);
+            this.start_bt.Name = "start_bt";
+            this.start_bt.Size = new System.Drawing.Size(174, 58);
+            this.start_bt.TabIndex = 9;
+            this.start_bt.Text = "Start";
+            this.start_bt.UseVisualStyleBackColor = true;
             // 
             // stop_button
             // 
-            this.stop_button.Location = new System.Drawing.Point(603, 343);
+            this.stop_button.Location = new System.Drawing.Point(601, 379);
             this.stop_button.Name = "stop_button";
             this.stop_button.Size = new System.Drawing.Size(174, 58);
             this.stop_button.TabIndex = 10;
             this.stop_button.Text = "Stop";
             this.stop_button.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // port_tb
+            // 
+            this.port_tb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.port_tb.Location = new System.Drawing.Point(232, 241);
+            this.port_tb.Multiline = true;
+            this.port_tb.Name = "port_tb";
+            this.port_tb.Size = new System.Drawing.Size(172, 41);
+            this.port_tb.TabIndex = 22;
+            this.port_tb.Text = "8080";
+            // 
+            // port_lb
+            // 
+            this.port_lb.AutoSize = true;
+            this.port_lb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.port_lb.Location = new System.Drawing.Point(129, 244);
+            this.port_lb.Name = "port_lb";
+            this.port_lb.Size = new System.Drawing.Size(40, 20);
+            this.port_lb.TabIndex = 21;
+            this.port_lb.Text = "Port";
+            // 
+            // Server
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(913, 500);
+            this.Controls.Add(this.port_tb);
+            this.Controls.Add(this.port_lb);
             this.Controls.Add(this.stop_button);
-            this.Controls.Add(this.listen_bt);
+            this.Controls.Add(this.start_bt);
             this.Controls.Add(this.banner_pic);
             this.Controls.Add(this.log_tb);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Name = "Form1";
+            this.Name = "Server";
             this.Text = "Server";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.banner_pic)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -97,8 +118,10 @@
         #endregion
         private System.Windows.Forms.TextBox log_tb;
         private System.Windows.Forms.PictureBox banner_pic;
-        private System.Windows.Forms.Button listen_bt;
+        private System.Windows.Forms.Button start_bt;
         private System.Windows.Forms.Button stop_button;
+        private System.Windows.Forms.TextBox port_tb;
+        private System.Windows.Forms.Label port_lb;
     }
 }
 
